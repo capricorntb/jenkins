ace033580539d78ff5583f763976799b3f7814ec
capricorntb <yintb@aliyun.com>
2017-12-20 10:48:47 +0800

    test1

--- src/main/java/boy/demo/helloController.java
+++ src/main/java/boy/demo/helloController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class helloController {
     @RequestMapping(value = "/hello", method = RequestMethod.GET)
     public String say(){
-        return "hello winter,let's snowboarding!!!";
+        return "hello github,let's snowboarding!!!";
     }
 
 
