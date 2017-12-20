f2ffa4151d566857c1d466b909af813717e87967
capricorntb <yintb@aliyun.com>
2017-12-20 10:26:15 +0800

    test

---
+++ src/main/java/boy/demo/helloController.java
@@ -0,0 +1,17 @@
+package boy.demo;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class helloController {
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    public String say(){
+        return "hello winter,let's snowboarding!!!";
+    }
+
+
+}
+
